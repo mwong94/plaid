e0d9865e9ac80b74750ffeb8882c3f15412709ac
Max Wong <wong.max94@gmail.com>
2024-08-06 11:49:19 -0700

    rename

--- src/create_deployment.py
+++ src/create_deployment.py
@@ -6,7 +6,7 @@ SOURCE_REPO='https://github.com/mwong94/plaid.git'
 if __name__ == '__main__':
     flow.from_source(
         source=SOURCE_REPO,
-        entrypoint='src/flows/plaid.py:get_institutions',
+        entrypoint='src/flows/plaid_extractor.py:get_institutions',
     ).deploy(
         name='plaid-deployment',
         work_pool_name='default-work-pool',
