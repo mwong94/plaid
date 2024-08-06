2fbedad63807d2692cedccb936b98017fc207140
Max Wong <wong.max94@gmail.com>
2024-08-06 11:41:57 -0700

    use CronSchedule

--- src/create_deployment.py
+++ src/create_deployment.py
@@ -1,4 +1,5 @@
 from prefect import flow
+from prefect.client.schemas.schedules import CronSchedule
 
 SOURCE_REPO='https://github.com/mwong94/plaid.git'
 
@@ -7,7 +8,10 @@ if __name__ == '__main__':
         source=SOURCE_REPO,
         entrypoint='src/flows/plaid.py:get_institutions',
     ).deploy(
-        name='my-first-deployment',
+        name='plaid-deployment',
         work_pool_name='default-work-pool',
-        cron='0 0 * * 0', # crontab.guru: “At 00:00 on Sunday.”
+        schedule=CronSchedule(
+            cron='0 0 * * 0', # crontab.guru: “At 00:00 on Sunday.”
+            timezone='US/Pacific'
+        )
     )
