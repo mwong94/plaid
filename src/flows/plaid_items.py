9858d8f8e9bd491aa7b9fe372a9bb8a1b282d1ae
Max Wong <wong.max94@gmail.com>
2024-08-07 10:53:01 -0700

    add logging

--- src/flows/plaid_items.py
+++ src/flows/plaid_items.py
@@ -1,10 +1,11 @@
 from prefect import flow, get_run_logger
 from prefect_snowflake.database import SnowflakeConnector
-from snowflake.connector.pandas_tools import write_pandas
 from textwrap import dedent
 
 @flow
 def add_item(item_id: str, access_token: str, institution_id: str, institution_name: str):
+    logger = get_run_logger()
+    logger.debug('add_item()')
     with SnowflakeConnector.load('sf1').get_connection() as conn:
         with conn.cursor() as cur:
             cur.execute(dedent('''
