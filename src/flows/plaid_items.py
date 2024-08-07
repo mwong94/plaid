884d51bad1aab7f47a3b887cbff9afe2b2591194
Max Wong <wong.max94@gmail.com>
2024-08-07 10:54:13 -0700

    fix parenthesis

--- src/flows/plaid_items.py
+++ src/flows/plaid_items.py
@@ -12,9 +12,9 @@ def add_item(item_id: str, access_token: str, institution_id: str, institution_n
                 insert into items
                     values
                     (%(item_id)s, %(access_token)s, %(institution_id)s, %(institution_name)s);
-                ''', {
+                '''), {
                     'item_id': item_id,
                     'access_token': access_token,
                     'institution_id': institution_id,
                     'institution_name': institution_name
-                }))
+                })
