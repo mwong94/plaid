a4df52af1890e26ab4065802e2464212bdf70bb7
Max Wong <wong.max94@gmail.com>
2024-08-07 16:31:19 -0700

    refactor commas

--- src/dbt/models/marts/transactions.sql
+++ src/dbt/models/marts/transactions.sql
@@ -1,53 +1,53 @@
 select
-    t.account_id,
-    a.bank_name,
-    t.account_owner,
-    t.amount,
-    t.authorized_date,
-    t.authorized_datetime,
-    t.category_0,
-    t.category_1,
-    t.category_2,
-    t.categories,
-    t.category_id,
-    t.check_number,
-    t.counterparties,
-    t.transaction_date,
-    t.datetime,
-    t.iso_currency_code,
-    t.location,
-    t.location__address,
-    t.location__city,
-    t.location__country,
-    t.location__lat,
-    t.location__lon,
-    t.location__postal_code,
-    t.location__region,
-    t.location__store_number,
-    t.logo_url,
-    t.merchant_entity_id,
-    t.merchant_name,
-    t.name,
-    t.payment_channel,
-    t.payment_meta__by_order_of,
-    t.payment_meta__payee,
-    t.payment_meta__payer,
-    t.payment_meta__payment_method,
-    t.payment_meta__payment_processor,
-    t.payment_meta__ppd_id,
-    t.payment_meta__reason,
-    t.payment_meta__reference_number,
-    t.pending,
-    t.pending_transaction_id,
-    t.personal_finance_category__confidence_level,
-    t.personal_finance_category__detailed,
-    t.personal_finance_category__primary,
-    t.personal_finance_category_icon_url,
-    t.transaction_code,
-    t.transaction_id,
-    t.transaction_type,
-    t.unofficial_currency_code,
-    t.website
+    t.account_id
+    , a.bank_name
+    , t.account_owner
+    , t.amount
+    , t.authorized_date
+    , t.authorized_datetime
+    , t.category_0
+    , t.category_1
+    , t.category_2
+    , t.categories
+    , t.category_id
+    , t.check_number
+    , t.counterparties
+    , t.transaction_date
+    , t.datetime
+    , t.iso_currency_code
+    , t.location
+    , t.location__address
+    , t.location__city
+    , t.location__country
+    , t.location__lat
+    , t.location__lon
+    , t.location__postal_code
+    , t.location__region
+    , t.location__store_number
+    , t.logo_url
+    , t.merchant_entity_id
+    , t.merchant_name
+    , t.name
+    , t.payment_channel
+    , t.payment_meta__by_order_of
+    , t.payment_meta__payee
+    , t.payment_meta__payer
+    , t.payment_meta__payment_method
+    , t.payment_meta__payment_processor
+    , t.payment_meta__ppd_id
+    , t.payment_meta__reason
+    , t.payment_meta__reference_number
+    , t.pending
+    , t.pending_transaction_id
+    , t.personal_finance_category__confidence_level
+    , t.personal_finance_category__detailed
+    , t.personal_finance_category__primary
+    , t.personal_finance_category_icon_url
+    , t.transaction_code
+    , t.transaction_id
+    , t.transaction_type
+    , t.unofficial_currency_code
+    , t.website
 
 from {{ ref('stg_transactions') }} as t
 
