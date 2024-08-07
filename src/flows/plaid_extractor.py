99f805cf2775608a2cd0239df8cc6b5a8e7fd90f
Max Wong <wong.max94@gmail.com>
2024-08-07 10:00:17 -0700

    move generic plaid tasks to separate file

--- src/flows/plaid_extractor.py
+++ src/flows/plaid_extractor.py
@@ -1,8 +1,5 @@
 from prefect import flow, task, get_run_logger
-from prefect.variables import Variable
-from prefect.artifacts import create_table_artifact, create_markdown_artifact
-from prefect_snowflake.database import SnowflakeConnector
-from snowflake.connector.pandas_tools import write_pandas, pd_writer
+from prefect.artifacts import create_markdown_artifact
 
 import os
 import pandas as pd
@@ -10,7 +7,6 @@ from datetime import datetime
 
 from utils import RateLimiter, cast_to_string
 
-import plaid
 from plaid.api import plaid_api
 from plaid.model.accounts_get_request import AccountsGetRequest
 from plaid.model.accounts_get_request_options import AccountsGetRequestOptions
@@ -19,27 +15,6 @@ from plaid.model.institutions_get_request import InstitutionsGetRequest
 from plaid.model.institutions_get_request_options import InstitutionsGetRequestOptions
 from plaid.model.transactions_sync_request import TransactionsSyncRequest
 from plaid.model.transactions_sync_request_options import TransactionsSyncRequestOptions
-
-
-@task(retries=2)
-def create_client() -> plaid_api.PlaidApi:
-    logger = get_run_logger()
-    logger.debug('create_client()')
-
-    client_id = os.getenv('PLAID_CLIENT_ID')
-    secret = os.getenv('PLAID_SECRET')
-    version = os.getenv('PLAID_API_VERSION')
-    configuration = plaid.Configuration(
-        host=plaid.Environment.Production,
-        api_key={
-            'clientId': client_id,
-            'secret': secret,
-            'plaidVersion': version
-        }
-    )
-
-    api_client = plaid.ApiClient(configuration)
-    return plaid_api.PlaidApi(api_client)
 
 
 @task(retries=5)
@@ -69,24 +44,12 @@ def _get_institutions(client = plaid_api.PlaidApi, debug: bool = False) -> pd.Da
             break
 
     df = pd.DataFrame(institutions)
+    df['loaded_at'] = datetime.utcnow()
+    for col in df.columns:
+        df[col] = df[col].apply(cast_to_string)
+    df = df[['institution_id', 'name', 'products', 'country_codes', 'routing_numbers', 'oauth', 'loaded_at']]
 
     return df
-
-
-@task(retries=2)
-def upload_df(df: pd.DataFrame, schema: str, table: str, delete: bool = False) -> None:
-    with SnowflakeConnector.load('sf1').get_connection() as conn:
-        if delete:
-            with conn.cursor() as cur:
-                cur.execute(f'delete from {schema}.{table}')
-        write_pandas(
-            conn,
-            df,
-            table,
-            database='PLAID',
-            schema=schema,
-            quote_identifiers=False
-        )
 
 
 @flow
@@ -95,13 +58,7 @@ def get_institutions(debug: bool = False, delete: bool = False) -> None:
     logger.debug('get_institutions()')
 
     client = create_client()
-
     df = _get_institutions(client, debug)
-    df['loaded_at'] = datetime.utcnow()
-    for col in df.columns:
-        df[col] = df[col].apply(cast_to_string)
-    df = df[['institution_id', 'name', 'products', 'country_codes', 'routing_numbers', 'oauth', 'loaded_at']]
-    
     upload_df(df, 'raw', 'institutions', True)
 
     create_markdown_artifact(
---
+++ src/flows/plaid_tasks.py
@@ -0,0 +1,48 @@
+from prefect import task, get_run_logger
+from prefect_snowflake.database import SnowflakeConnector
+from snowflake.connector.pandas_tools import write_pandas
+
+import os
+import pandas as pd
+
+import plaid
+from plaid.api import plaid_api
+
+
+@task(retries=2)
+def create_client() -> plaid_api.PlaidApi:
+    logger = get_run_logger()
+    logger.debug('create_client()')
+
+    client_id = os.getenv('PLAID_CLIENT_ID')
+    secret = os.getenv('PLAID_SECRET')
+    version = os.getenv('PLAID_API_VERSION')
+    configuration = plaid.Configuration(
+        host=plaid.Environment.Production,
+        api_key={
+            'clientId': client_id,
+            'secret': secret,
+            'plaidVersion': version
+        }
+    )
+
+    api_client = plaid.ApiClient(configuration)
+    return plaid_api.PlaidApi(api_client)
+
+
+@task(retries=2)
+def upload_df(df: pd.DataFrame, schema: str, table: str, delete: bool = False) -> None:
+    with SnowflakeConnector.load('sf1').get_connection() as conn:
+        if delete:
+            with conn.cursor() as cur:
+                cur.execute(f'delete from {schema}.{table}')
+        write_pandas(
+            conn,
+            df,
+            table,
+            database='PLAID',
+            schema=schema,
+            quote_identifiers=False
+        )
+
+
